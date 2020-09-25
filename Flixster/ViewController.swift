c6c8129ddea1de9d32a3c62bfffafd5e5f92c641
Naomi Amuzie <ncaa2018@mymail.pomona.edu>
2020-09-24 22:22:56 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by loan on 9/24/20.
+//  Copyright © 2020 naomia2022@hotmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
